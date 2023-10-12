8265d2408fff44ce5eff3da8df91b13f9e5ad10f
SumitSingh2023 <121820847+SumitSingh2023@users.noreply.github.com>
2023-10-12 16:51:51 +0530

    Add files via upload

---
+++ problem2.js
@@ -0,0 +1,20 @@
+/*Write code to find the absolute difference of two numbers
+
+Sample Input-1 12,4
+Sample Output-1 8
+Sample Input-2 4,18
+Sample Output-2 14*/
+
+function diffrence(a,b){
+    return a-b
+}
+
+let output=diffrence(12,4);
+console.log(output);
+
+function diff(a,b){
+    return a-b
+
+}
+let outpu=diff(4,18);
+console.log(Math.abs(outpu))
